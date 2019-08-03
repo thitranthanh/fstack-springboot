287feee16fa1b7f6dc04e9370b503ba1a48f2f3b
TiKing <thitranthanh@gmail.com>
2019-08-03 21:51:04 +0700

    spring boot "hello world"

---
+++ src/main/java/vn/fstack/springboot/controller/HelloController.java
@@ -0,0 +1,16 @@
+package vn.fstack.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+	@RequestMapping("/")
+	@ResponseBody
+	public String getHelloWorld() {
+		return "Hello World!";
+	}
+
+}
